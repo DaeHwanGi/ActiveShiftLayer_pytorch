edc07bd9edf848a37fcd45c0513756b408be9664
DaeHwan Gi <dhgi0630@gmail.com>
2019-06-15 18:20:37 +0900

    Update ActiveShift2d.py

--- ActiveShift2d.py
+++ ActiveShift2d.py
@@ -1,63 +1,41 @@
 import torch
 import torch.nn as nn
+import DepthwiseAffineGrid
+import DepthwiseGridSampler
 import numpy as np
 
-class shift(torch.autograd.Function):
+class DepthwiseAffineGridFunction(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x, theta):
-        ctx.save_for_backward(x)
-        B, C, H, W = x.size()
-        theta = -theta
-        for c in range(C):
-            m, n = np.indices((H, W))
-            m = torch.from_numpy(m).long()
-            n = torch.from_numpy(n).long()
-            floor_theta = torch.floor(theta[c, :]).long()
-            delta_theta = theta[c, :] - floor_theta.float()
-
-            x0 = n + floor_theta[1]
-            x1 = n + floor_theta[1] + 1
-            y0 = m + floor_theta[0]
-            y1 = m + floor_theta[0] + 1
-
-            x0 = torch.clamp(x0, 0, W - 1)[1:-1, 1:-1]
-            x1 = torch.clamp(x1, 0, W - 1)[1:-1, 1:-1]
-            y0 = torch.clamp(y0, 0, H - 1)[1:-1, 1:-1]
-            y1 = torch.clamp(y1, 0, H - 1)[1:-1, 1:-1]
-
-            z1 = x[:, c:c + 1, y0, x0]
-            z2 = x[:, c:c + 1, y0, x1]
-            z3 = x[:, c:c + 1, y1, x0]
-            z4 = x[:, c:c + 1, y1, x1]
-
-            x[:, c:c + 1, 1:-1, 1:-1] = z1 * (1 - delta_theta[0]) * (1 - delta_theta[1]) + z2 * (1 - delta_theta[0]) * \
-                                        delta_theta[1] \
-                                        + z3 * delta_theta[0] * (1 - delta_theta[1]) + z4 * delta_theta[0] * \
-                                        delta_theta[1]
-        return x[:, :, 1:-1, 1:-1]
+    def forward(ctx, theta, size):
+        ctx.intermediate_results = size
+        return DepthwiseAffineGrid.forward(theta, size)
     @staticmethod
-    def backward(ctx, grad_output):
-        pass
+    def backward(ctx, grad):
+        size = ctx.intermediate_results
+        return DepthwiseAffineGrid.backward(grad, size), None
+
+class DepthwiseGridSamplerFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, grid):
+        ctx.save_for_backward(input, grid)
+        return DepthwiseGridSampler.forward(input, grid)
+    @staticmethod
+    def backward(ctx, grad):
+        input, grid = ctx.saved_variables
+        return DepthwiseGridSampler.backward(grad, input, grid)
+
 
 class ActiveShift2d(nn.Module):
     def __init__(self, in_channel, out_channel, bias=False):
         super(ActiveShift2d, self).__init__()
-        self.theta_s = nn.Parameter(torch.FloatTensor(in_channel, 2))
-        self.theta_s.data.uniform_(-1, 1)
-        self.zeropad = nn.ZeroPad2d(1)
-        self.depthwise = nn.Conv2d(in_channel, in_channel, kernel_size=1, bias=bias, groups=in_channel)
+        self.theta = nn.Parameter(torch.FloatTensor(in_channel, 2).unsqueeze_(-1))
+        self.theta.data.uniform_(-0.1, 0.1) # have to change
+        self.base_theta = torch.FloatTensor([[1, 0], [0, 1]]).repeat(in_channel, 1, 1)
         self.pointwise = nn.Conv2d(in_channel, out_channel, kernel_size=1, bias=bias)
 
     def forward(self, x):
-        x = self.zeropad(x)
-        x = shift.apply(x, self.theta_s)
-        x = self.depthwise(x)
-        return self.pointwise(x)
-
-if __name__ == '__main__':
-    x = torch.FloatTensor(16, 3, 224, 224).cuda()
-    theta_s = torch.FloatTensor(3, 2)
-    layer = ActiveShift2d(3, 5).cuda()
-    with torch.autograd.profiler.profile(use_cuda=True) as prof:
-        x = shift.apply(x, theta_s)
-    print(prof)
+        theta = torch.cat([self.base_theta.type_as(self.theta), self.theta], dim=-1)
+        grid = DepthwiseAffineGridFunction.apply(theta, x.size())
+        shifted = DepthwiseGridSamplerFunction.apply(x.contiguous(), grid.contiguous())
+        out = self.pointwise(shifted)
+        return out
